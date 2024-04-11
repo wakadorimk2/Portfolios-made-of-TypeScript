af9bcdcac83f120d65dad36a2df0567d29cc8d1c
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-11 14:22:30 +0900

    アイコンの配置を修正

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center place-items-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <SocialButton icon="./x-logo.svg" url="https://twitter.com/wakadori_Mk3" alt="x logo icon"/>
 
         <a
