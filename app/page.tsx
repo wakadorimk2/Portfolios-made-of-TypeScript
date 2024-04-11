1bc7b7ca5002607f8a92ab3eaf38ac0f362d66fb
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-11 14:21:20 +0900

    ボタンを追加

--- app/page.tsx
+++ app/page.tsx
@@ -30,22 +30,7 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://twitter.com/wakadori_Mk3"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            <SocialButton icon="./x-logo.svg" url="https://twitter.com/wakadori_Mk3" alt="x logo icon"/>
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Look at my beautiful girl illustrations!
-          </p>
-        </a>
+        <SocialButton icon="./x-logo.svg" url="https://twitter.com/wakadori_Mk3" alt="x logo icon"/>
 
         <a
           href="https://www.pixiv.net/users/75962333"
